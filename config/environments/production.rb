e8a76dbfc63067ac943644adb24b390b80031375
Ivan Necas <necasik@gmail.com>
2013-05-09 14:33:47 +0200

    Fallback to assets pipeline if a precompiled asset is missed
    That's the case when an engine with it's own assets is added to
    Katello later in production. Especially for images.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Src::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
